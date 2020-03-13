0651194300ed3fef1eb7c8f2bb6487a634fac8bb
l-sugar <l.svugr@gmail.com>
2020-03-13 16:34:57 -0700

    upperacse PORT variable

--- app.js
+++ app.js
@@ -6,17 +6,11 @@ const morgan = require('morgan');
 const data = require('./data');
 const bot = require('./bot').bot;
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(morgan('dev'));
-
-app.get('/', async (req, res, next) => {
-	let result = await data.fullDataSet();
-	console.log(result);
-	res.send(result);
-});
 
 app.listen(PORT, (req, res, next) => {
 	console.log('Server listening on port 3000');
