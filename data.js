448f24ad3bbcb1592ca75f598b13613f76d84da6
l-sugar <l.svugr@gmail.com>
2020-03-18 22:29:37 +0100

    added my own npm package for testing

--- data.js
+++ data.js
@@ -1,17 +1,17 @@
 const axios = require('axios');
 const util = require('util');
-const covid = require('novelcovid');
+const covid = require('@louis.sugar/covid19');
 
 const all = async () => {
 	return await covid.all();
 };
 
 const country = async country => {
-	let data = await covid.countries();
-	for (let entry of data) {
-		if (entry.country.toLowerCase() === country.toLowerCase()) {
-			return entry;
-		}
+	let data = await covid.countries(country);
+	if (data) {
+		return data;
+	} else {
+		return false;
 	}
 };
 
