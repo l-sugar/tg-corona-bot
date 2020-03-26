6849c8318040e6d79f5432a07c2654ef01693360
l-sugar <l.svugr@gmail.com>
2020-03-26 13:42:49 +0100

    additional loggin

--- data.js
+++ data.js
@@ -13,13 +13,16 @@ const all = async () => {
 };
 
 const country = async country => {
+	console.log(
+		`have received country, now passing into covid package: ${country}`
+	);
 	covid
 		.countries(country)
 		.then(data => {
 			console.log(`have received data: ${util.inspect(data)}`);
 			return data;
 		})
-		.catch(err => console.error(err));
+		.catch(err => console.error(`ran into error when retrieving data: ${err}`));
 };
 
 module.exports = {
